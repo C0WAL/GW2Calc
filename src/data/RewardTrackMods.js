f00fdd86f9e38b4eee16216eea4eb7569a476e84
C0WAL <zidicck@gmail.com>
2024-12-18 13:13:29 +0100

    Added basic resources for reward track calculator

---
+++ src/data/RewardTrackMods.js
@@ -0,0 +1,87 @@
+export const BasicRewardTrackBonuses = [
+    {
+        name: 'Experience Booster',
+        icon: 'xp-bonus.png',
+        value: 50
+    },
+    {
+        name: 'Black Lion Booster',
+        icon: 'bl-bonus.png',
+        value: 25
+    },
+    {
+        name: 'Festival Gobbler Boost',
+        icon: 'festival-gobbler-bonus.png',
+        value: 25
+    },
+    {
+        name: 'Celebration Bonus',
+        icon: 'celebration-bonus.png',
+        value: 10
+    },
+    {
+        name: 'WvW Reward Track Enrichment',
+        icon: 'wvw-rewardtrack-enrichment.png',
+        value: 5
+    }
+]
+
+export const RestedInComfyHome = [
+    {
+        name: 'A Little Well Rested in a Comfy Home',
+        value: 2
+    },
+    {
+        name: 'Well Rested in a Comfy Home',
+        value: 4
+    },
+    {
+        name: 'Very Well Rested in a Comfy Home',
+        value: 6
+    },
+    {
+        name: 'Highly Well Rested in a Comfy Home',
+        value: 8
+    },
+]
+
+export const WvWGuildEnhancement = [
+    {
+        tier: 1,
+        value: 3
+    },
+    {
+        tier: 2,
+        value: 4
+    }
+    ,
+    {
+        tier: 3,
+        value: 5
+    }
+    ,
+    {
+        tier: 4,
+        value: 6
+    }
+    ,
+    {
+        tier: 5,
+        value: 7
+    }
+    ,
+    {
+        tier: 6,
+        value: 8
+    }
+    ,
+    {
+        tier: 7,
+        value: 9
+    }
+    ,
+    {
+        tier: 8,
+        value: 10
+    }
+]
