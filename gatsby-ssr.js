295af0e0b127334f651b78ac39652b6785ce41c7
C0WAL <zidicck@gmail.com>
2025-01-15 11:23:09 +0100

    Changes to dark-mode-script in head 2

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,15 +8,17 @@ export const onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
           dangerouslySetInnerHTML={{
             __html: `
               (function() {
-                const storedDarkMode = localStorage.getItem("darkMode");
-                const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-                
-                const darkMode = storedDarkMode === "true" || (storedDarkMode === null && prefersDark);
-                localStorage.setItem("darkMode", darkMode);
-                if (darkMode) {
-                  document.documentElement.classList.add("dark");
-                } else {
-                  document.documentElement.classList.remove("dark");
+                if (typeof window !== "undefined" && window.localStorage) {
+                  const storedDarkMode = localStorage.getItem("darkMode");
+                  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+                  
+                  const darkMode = storedDarkMode === "true" || (storedDarkMode === null && prefersDark);
+                  localStorage.setItem("darkMode", darkMode);
+                  if (darkMode) {
+                    document.documentElement.classList.add("dark");
+                  } else {
+                    document.documentElement.classList.remove("dark");
+                  }
                 }
               })();
             `,
